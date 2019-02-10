5f746bb47c6046c962825af8fd1a8f7661455ddb
sofiagarkot <sofia.garkot2001@gmail.com>
2019-02-10 20:35:06 +0200

    sofia task2 lab2

--- 2_second_task.py
+++ 2_second_task.py
@@ -3,6 +3,10 @@ from tqdm import tqdm
 import geocoder
 
 def read_file(path):
+    ''' (str) -> list[tuple(str,str,str)]
+    Reads the file and returns the list of tuples. Each tuple contains string, that are : name of film,
+    year it was made and location.
+    '''
     f = open(path,encoding = "ISO-8859-1")
     lines = f.readlines()
     res = []
@@ -21,10 +25,14 @@ def read_file(path):
 
 
 def number_generator(lst):
+    ''' (list[tuple(str,str)]) -> dictionary
+    Returns dictionary, where keys are locations (last element of the tuple) and values are numbers of films,
+    that were filmed on this location.
+    '''
     dct = {}
     i=0
-    for tup in lst:
-        if i < 1000:
+    for tup in tqdm(lst):
+        if i <= 1000:
             if tup[-1] not in dct.keys():
                 dct[tup[-1]] = 1
             else:
@@ -35,15 +43,23 @@ def number_generator(lst):
 
 
 def color_creator(number):
+    '''(int) -> str
+    Returns colour depending on number.
+    >>> color_creator(3)
+    'green'
+    '''
     if number < 5:
         return "green"
-    elif 5 <= number <= 23:
+    elif 5 <= number <= 20:
         return "yellow"
     else:
         return "red"
 
 
 def map_population(json_file):
+    '''(str) -> folium.map.FeatureGroup
+    Returns new layer of the map depending on the data given in json file.
+    '''
     fg_pp = folium.FeatureGroup(name="Population")
     fg_pp.add_child(folium.GeoJson(data=open(json_file, 'r',
                                              encoding='utf-8-sig').read(),
@@ -51,19 +67,27 @@ def map_population(json_file):
                                    if x['properties']['POP2005'] < 10000000
                                    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
                                    else 'red'}))
-
     return(fg_pp)
 
 def marker_film_location(lst):
+    ''' (list[tuple(str,str,str)]) -> folium.map.FeatureGroup
+    Returns new layer of the map, that is locations of the movies, filmed in some year, that user has typed.
+    '''
     fg_film = folium.FeatureGroup(name="Films")
-    user_wish = input("Enter year : ")
-
+    i = 0
+    while i < 5:
+        try:
+            user_wish = int(input("Enter a year, you want films to be displayed on the map : "))
+            break
+        except:
+            i+=1
+    user_wish = str(user_wish)
     def detect(tup):
         return tup[1] == user_wish
 
     res1 = list(filter(detect, lst))
-    if len(res1) > 300:
-        res1 = res1[:300]
+    if len(res1) > 500:
+        res1 = res1[:500]
     index_of_location = 0
     for point in tqdm(list(t[-1] for t in res1)):
         location = geocoder.arcgis(point).latlng
@@ -74,6 +98,10 @@ def marker_film_location(lst):
 
 
 def point_film(dct):
+    '''(dictionary) -> folium.map.FeatureGroup
+    Returns a new layer of the map. There are circle markerson it, coloured in different colours, depending on number
+    of movies(values), that were filmed in current location(keys).
+    '''
     fg_num = folium.FeatureGroup(name="Numbers")
     for key in tqdm(dct.keys()):
         location = geocoder.arcgis(key).latlng
@@ -87,7 +115,7 @@ def point_film(dct):
     return(fg_num)
 
 if __name__ == "__main__":
-    map = folium.Map(location=[46.8, 8.33], zoom_start=5)
+    map = folium.Map(location=[46.8, 8.33])
     map.add_child(map_population("world.json"))
     map.add_child(marker_film_location(read_file("locations.list")))
     map.add_child(point_film(number_generator(read_file("locations.list"))))
